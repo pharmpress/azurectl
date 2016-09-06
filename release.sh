b21597f70165964ff180ba5fd3772d3fa54d721d
rrramiro <ramiro.calle@rpharms.com>
2016-09-06 09:44:42 +0100

    Add release script

---
+++ release.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+version=$(grep -E -o "[0-9]+\.[0-9]+\.[0-9]+\+git" ./version/version.go 
+)
+
+if [[ "$version" =~ ^(.*\.)([0-9]+)\+git$ ]];
+then
+	inc=$((BASH_REMATCH[2]+1))
+	currentversion="${BASH_REMATCH[1]}${BASH_REMATCH[2]}";
+	nextversion="${BASH_REMATCH[1]}$inc";
+
+	git tag -l "v$currentversion"
+	git push origin "v$currentversion"
+	sed -i -e "s/${currentversion}/${nextversion}/" ./version/version.go
+	git add ./version/version.go
+	git commit -m "Setting version to ${nextversion}"
+	git push
+else
+	echo "Version wrong format"
+	exit 1
+fi
